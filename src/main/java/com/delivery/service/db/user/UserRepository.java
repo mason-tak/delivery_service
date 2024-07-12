f14e89422f2ec2b67bacc9dcd2de49817c176268
mason <conficker77@gmail.com>
2024-07-13 06:26:58 +0900

    필요한 쿼리 메서드 작성 - 특정 사용자 찾는 쿼리 - 로그인시 필요한 쿼리

--- src/main/java/com/delivery/service/db/user/UserRepository.java
+++ src/main/java/com/delivery/service/db/user/UserRepository.java
@@ -6,5 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    // 특정 사용자 찾기
+    // select * from user where id = ? and status = ? order by id desc limit 1;
+    Optional<UserEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, UserStatus userStatus);
 
+    // 로그인 할때
+    // select * from user where email = ? and password = ? and status = ? order by id desc limit 1;
+    Optional<UserEntity> findFirstByEmailAndPasswordAndStatusOrderByIdDesc(String email, String password, UserStatus userStatus);
 }
